dcd39efdd9e857ef566eb7083dc949ece9eb8366
GabrieleOx <ossolagabriele@gmail.com>
2025-02-09 11:43:02 +0100

    Prova di classe senza progetto

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@
 class Main{
     public static void main(String [] args){
         System.out.println("Hello World");
+        PrimaClasse x = new PrimaClasse();
+        x.setX(12);
+        System.out.println("Valore di x: " + x.getX());
     }
 }
