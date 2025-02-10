ade9255496e64e3b344074974d1d53e83b01199d
GabrieleOx <ossolagabriele@gmail.com>
2025-02-10 08:11:56 +0100

    Pulizia

--- Main.java
+++ Main.java
@@ -2,8 +2,5 @@
 class Main{
     public static void main(String [] args){
         System.out.println("Hello World");
-        PrimaClasse x = new PrimaClasse();
-        x.setX(12);
-        System.out.println("Valore di x: " + x.getX());
     }
 }
