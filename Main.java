44caf824ef17a128955a5b6894f03f8f30c1936c
spoltori <luca.spoltori@gmail.com>
2025-02-14 14:46:35 +0100

    ho fatto qualcosiam

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Main{
+    static boolean ret = true;
+
+    public static void changeRet(){
+        ret = !ret;
+    }
     public static void main(String [] args){ // Se vuoi provare qualcosa scrivi nel main questo: Quiz();
         while(Home());
         Quiz();
@@ -25,31 +30,42 @@ public class Main{
     }
 
     public static boolean Home(){ // qui mettiamo la schermata home, ritornerà false solo qundo si vuole uscire...
-        return false;
+        JDialog home= new JDialog((Frame) null, "home", true);//crea la finestra
+        JLabel titolo=new JLabel("IL VOLONTARIATO PER TUTTI");
+        JButton info= new JButton("INFORMAZIONI SUL VOLONTARIATO");
+         info.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                home.dispose();
+                Informazioni.showInfo();
+            }
+        });
+        home.setSize(600, 600);
+        home.add(titolo); home.add(info);
+        return ret;
     }
 
     public static void Quiz(){
         Question [] q = new Question[20]; //Le venti domande del quiz da settare:
-q[0] = new Question("Quale diritto ha un volontario?", "Diritto di ricevere formazione", "Dovere di impegnarsi", "Dovere di rispettare le regole", "Dovere di mantenere la riservatezza", 1);
-q[1] = new Question("Qual è un dovere di un volontario?", "Diritto di essere riconosciuti", "Dovere di impegnarsi", "Diritto alla sicurezza", "Diritto di essere trattati con rispetto", 2);
-q[2] = new Question("<html>Cosa significa il diritto alla <br>sicurezza per un volontario?</html>", "Essere trattati con rispetto", "Operare in un ambiente sicuro", "Rispettare le regole dell'organizzazione", "Partecipare a sessioni di formazione", 2);
-q[3] = new Question("<html>Quale comportamento è corretto per un volontario riguardo alla<br> riservatezza?</html>", "Condividere tutte le informazioni", "<html>Rispettare la riservatezza delle  <br>informazioni sensibili</html>", "Ignorare le politiche dell'organizzazione", "Condividere informazioni senza autorizzazione", 2);
-q[4] = new Question("<html>Cosa dovrebbe fare un volontario se non può più rispettare<br> un impegno preso?</html>", "Ignorare l'impegno", "<html>Informare l'organizzazione e cercare di trovare <br>una soluzione</html>", "Continuare senza informare nessuno", "Lasciare l'attività senza preavviso", 2);
-q[5] = new Question("<html>Quale diritto assicura che il volontario sia informato sugli obiettivi<br> dell'organizzazione?</html>", "Diritto alla sicurezza", "Diritto di essere riconosciuti", "Diritto di essere informati", "Diritto di ricevere formazione", 3);
-q[6] = new Question("<html>Qual è un dovere importante per mantenere un ambiente di lavoro<br> sicuro?</html>", "Ignorare le norme di sicurezza", "Rispettare le regole di sicurezza", "Lamentarsi delle condizioni di lavoro", "Evitare di seguire le indicazioni", 2);
-q[7] = new Question("<html>Quale diritto implica che i volontari debbano avere <br>accesso alle risorse necessarie?</html>", "Diritto di essere riconosciuti", "Diritto alla sicurezza", "Diritto di essere trattati con rispetto", "Diritto di formarsi", 4);
-q[8] = new Question("<html>Qual è uno dei benefici del riconoscimento dei volontari<br> da parte dell'organizzazione?</html>", "<html>Incrementa la motivazione <br>e l'impegno dei volontari</html>", "Riduce la produttività dei volontari", "Rende obbligatorio il volontariato", "Evita la formazione dei volontari", 1);
-q[9] = new Question("<html>Qual è un esempio di rispetto delle<br> regole dell'organizzazione?</html>", "Lavorare autonomamente senza guida", "Seguire le politiche e le procedure stabilite", "Evitare di comunicare con i superiori", "Ignorare le linee guida dell'organizzazione", 2);
-q[10] = new Question("<html>Perché è importante il dovere di impegnarsi<br> per un volontario?</html>", "<html>Per garantire che il lavoro sia completato <br> in modo affidabile</html>", "Per evitare responsabilità", "Per ridurre le interazioni con gli altri", "Per aumentare il tempo libero", 1);
-q[11] = new Question("<html>Quale diritto protegge i volontari dall'essere<br> maltrattati o discriminati?</html>", "Diritto alla sicurezza", "Diritto di essere trattati con rispetto", "Diritto di ricevere formazione", "Diritto di essere informati", 2);
-q[12] = new Question("<html>Qual è una pratica corretta per<br> mantenere la riservatezza delle informazioni?</html>", "Condividere i dati personali dei beneficiari", "Proteggere le informazioni sensibili", "Pubblicare le informazioni riservate online", "Ignorare le norme sulla riservatezza", 2);
-q[13] = new Question("<html>Quale dovere riguarda la partecipazione a sessioni<br> di formazione e aggiornamento?</html>", "Dovere di formarsi", "Dovere di impegnarsi", "Dovere di collaborare", "Dovere di mantenere la riservatezza", 1);
-q[14] = new Question("<html>Perché è importante che i volontari <br>collaborino con altri?</html>", "Per lavorare in isolamento", "Per creare un ambiente di lavoro armonioso", "Per evitare comunicazioni", "Per ignorare i suggerimenti degli altri", 2);
-q[15] = new Question("<html>Cosa significa il diritto di ricevere formazione<br> per un volontario?</html>", "Ottenere una retribuzione elevata", "<html>Ricevere una formazione adeguata per <br>svolgere il loro lavoro</html>", "Evitare qualsiasi tipo di istruzione", "Ignorare le opportunità di apprendimento", 2);
-q[16] = new Question("<html>Qual è un esempio di comportamento rispettoso per <br>un volontario?</html>", "Essere puntuali e affidabili", "Arrivare in ritardo senza avviso", "Ignorare le richieste dei superiori", "Lasciare i compiti incompleti", 1);
-q[17] = new Question("<html>Quale diritto garantisce che i volontari siano<br> riconosciuti per il loro contributo?</html>", "Diritto di essere riconosciuti", "Diritto alla sicurezza", "Diritto di formarsi", "Diritto di collaborare", 1);
-q[18] = new Question("<html>Quale dovere comporta il rispetto delle norme e<br> delle politiche dell'organizzazione?</html>", "Dovere di mantenere la riservatezza", "Dovere di rispettare le regole", "Dovere di formarsi", "Dovere di impegnarsi", 2);
-q[19] = new Question("<html>Perché è importante per i volontari mantenere <br>la riservatezza?</html>", "Per proteggere la privacy dei beneficiari", "Per condividere liberamente tutte le informazioni", "Per evitare di rispettare le norme", "Per pubblicare informazioni riservate", 1);
+        q[0] = new Question("Quale diritto ha un volontario?", "Diritto di ricevere formazione", "Dovere di impegnarsi", "Dovere di rispettare le regole", "Dovere di mantenere la riservatezza", 1);
+        q[1] = new Question("Qual è un dovere di un volontario?", "Diritto di essere riconosciuti", "Dovere di impegnarsi", "Diritto alla sicurezza", "Diritto di essere trattati con rispetto", 2);
+        q[2] = new Question("<html>Cosa significa il diritto alla <br>sicurezza per un volontario?</html>", "Essere trattati con rispetto", "Operare in un ambiente sicuro", "Rispettare le regole dell'organizzazione", "Partecipare a sessioni di formazione", 2);
+        q[3] = new Question("<html>Quale comportamento è corretto per un volontario riguardo alla<br> riservatezza?</html>", "Condividere tutte le informazioni", "<html>Rispettare la riservatezza delle  <br>informazioni sensibili</html>", "Ignorare le politiche dell'organizzazione", "Condividere informazioni senza autorizzazione", 2);
+        q[4] = new Question("<html>Cosa dovrebbe fare un volontario se non può più rispettare<br> un impegno preso?</html>", "Ignorare l'impegno", "<html>Informare l'organizzazione e cercare di trovare <br>una soluzione</html>", "Continuare senza informare nessuno", "Lasciare l'attività senza preavviso", 2);
+        q[5] = new Question("<html>Quale diritto assicura che il volontario sia informato sugli obiettivi<br> dell'organizzazione?</html>", "Diritto alla sicurezza", "Diritto di essere riconosciuti", "Diritto di essere informati", "Diritto di ricevere formazione", 3);
+        q[6] = new Question("<html>Qual è un dovere importante per mantenere un ambiente di lavoro<br> sicuro?</html>", "Ignorare le norme di sicurezza", "Rispettare le regole di sicurezza", "Lamentarsi delle condizioni di lavoro", "Evitare di seguire le indicazioni", 2);
+        q[7] = new Question("<html>Quale diritto implica che i volontari debbano avere <br>accesso alle risorse necessarie?</html>", "Diritto di essere riconosciuti", "Diritto alla sicurezza", "Diritto di essere trattati con rispetto", "Diritto di formarsi", 4);
+        q[8] = new Question("<html>Qual è uno dei benefici del riconoscimento dei volontari<br> da parte dell'organizzazione?</html>", "<html>Incrementa la motivazione <br>e l'impegno dei volontari</html>", "Riduce la produttività dei volontari", "Rende obbligatorio il volontariato", "Evita la formazione dei volontari", 1);
+        q[9] = new Question("<html>Qual è un esempio di rispetto delle<br> regole dell'organizzazione?</html>", "Lavorare autonomamente senza guida", "Seguire le politiche e le procedure stabilite", "Evitare di comunicare con i superiori", "Ignorare le linee guida dell'organizzazione", 2);
+        q[10] = new Question("<html>Perché è importante il dovere di impegnarsi<br> per un volontario?</html>", "<html>Per garantire che il lavoro sia completato <br> in modo affidabile</html>", "Per evitare responsabilità", "Per ridurre le interazioni con gli altri", "Per aumentare il tempo libero", 1);
+        q[11] = new Question("<html>Quale diritto protegge i volontari dall'essere<br> maltrattati o discriminati?</html>", "Diritto alla sicurezza", "Diritto di essere trattati con rispetto", "Diritto di ricevere formazione", "Diritto di essere informati", 2);
+        q[12] = new Question("<html>Qual è una pratica corretta per<br> mantenere la riservatezza delle informazioni?</html>", "Condividere i dati personali dei beneficiari", "Proteggere le informazioni sensibili", "Pubblicare le informazioni riservate online", "Ignorare le norme sulla riservatezza", 2);
+        q[13] = new Question("<html>Quale dovere riguarda la partecipazione a sessioni<br> di formazione e aggiornamento?</html>", "Dovere di formarsi", "Dovere di impegnarsi", "Dovere di collaborare", "Dovere di mantenere la riservatezza", 1);
+        q[14] = new Question("<html>Perché è importante che i volontari <br>collaborino con altri?</html>", "Per lavorare in isolamento", "Per creare un ambiente di lavoro armonioso", "Per evitare comunicazioni", "Per ignorare i suggerimenti degli altri", 2);
+        q[15] = new Question("<html>Cosa significa il diritto di ricevere formazione<br> per un volontario?</html>", "Ottenere una retribuzione elevata", "<html>Ricevere una formazione adeguata per <br>svolgere il loro lavoro</html>", "Evitare qualsiasi tipo di istruzione", "Ignorare le opportunità di apprendimento", 2);
+        q[16] = new Question("<html>Qual è un esempio di comportamento rispettoso per <br>un volontario?</html>", "Essere puntuali e affidabili", "Arrivare in ritardo senza avviso", "Ignorare le richieste dei superiori", "Lasciare i compiti incompleti", 1);
+        q[17] = new Question("<html>Quale diritto garantisce che i volontari siano<br> riconosciuti per il loro contributo?</html>", "Diritto di essere riconosciuti", "Diritto alla sicurezza", "Diritto di formarsi", "Diritto di collaborare", 1);
+        q[18] = new Question("<html>Quale dovere comporta il rispetto delle norme e<br> delle politiche dell'organizzazione?</html>", "Dovere di mantenere la riservatezza", "Dovere di rispettare le regole", "Dovere di formarsi", "Dovere di impegnarsi", 2);
+        q[19] = new Question("<html>Perché è importante per i volontari mantenere <br>la riservatezza?</html>", "Per proteggere la privacy dei beneficiari", "Per condividere liberamente tutte le informazioni", "Per evitare di rispettare le norme", "Per pubblicare informazioni riservate", 1);
 
         for(int j = 0; j < 20; j++) 
             q[j].quiz();
