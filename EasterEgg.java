6b69f48c674479e85e61a4df7dc76302c9cce345
spoltori <luca.spoltori@gmail.com>
2025-02-16 22:35:04 +0100

    ho creato al finstra EasterEgg e ho aggiuntoillink

--- EasterEgg.java
+++ EasterEgg.java
@@ -1,7 +1,36 @@
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.URI;
+import javax.swing.*;
+
 public class EasterEgg {
 
-    public static void showInfo(){ //Qui puoi fare la scheda con i link ai video e quello che vuoi...
-        
+    public static void showInfo(){ 
+        JDialog easteregg = new JDialog((Frame) null, "easteregg", true);
+        easteregg.setLayout(null);
+        easteregg.setSize(600, 600);
+        easteregg.getContentPane().setBackground(new Color(120, 66, 100));
+
+        JLabel linkYT = new JLabel("<html><a href=''>Visita il nostro sito</a></html>");
+        linkYT.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        linkYT.setBounds(50, 50, 200, 30);
+        linkYT.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI("https://youtu.be/WN18kGdPHzk?si=Ktjt_M95ovATLzh6"));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        easteregg.add(linkYT);
+        easteregg.setVisible(true);
     }
-    
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(EasterEgg::showInfo);
+    }
 }
