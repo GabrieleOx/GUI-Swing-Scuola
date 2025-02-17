804f62462ae6e02d594c3e9d8ed43589f727999a
spoltori <luca.spoltori@gmail.com>
2025-02-17 22:26:53 +0100

    MILGIORATO LA GRAFICA

--- EasterEgg.java
+++ EasterEgg.java
@@ -10,11 +10,11 @@ public class EasterEgg {
         JDialog easteregg = new JDialog((Frame) null, "easteregg", true);
         easteregg.setLayout(null);
         easteregg.setSize(600, 600);
-        easteregg.getContentPane().setBackground(new Color(120, 66, 100));
+        easteregg.getContentPane().setBackground(new Color(133, 189, 117));
 
         JLabel linkYT = new JLabel("<html><a href=''>video di sensibilizzazione</a></html>");
         linkYT.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        linkYT.setBounds(210, 220, 200, 30);
+        linkYT.setBounds(210, 360, 200, 30);
         linkYT.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -27,7 +27,7 @@ public class EasterEgg {
         });
         JLabel link1 = new JLabel("<html><a href=''>volotariato internazionale</a></html>");
         link1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        link1.setBounds(210, 150, 200, 30);
+        link1.setBounds(210, 125, 200, 30);
         link1.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -40,7 +40,7 @@ public class EasterEgg {
         });
         JLabel link2 = new JLabel("<html><a href=''>volotariato per i giovani</a></html>");
         link2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        link2.setBounds(210, 150, 200, 30);
+        link2.setBounds(213, 185, 200, 30);
         link2.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -51,8 +51,13 @@ public class EasterEgg {
                 }
             }
         });
-        
-        easteregg.add(linkYT);easteregg.add(link1);
+        JLabel titololink=new JLabel("PER AVVICINARSI AL VOLONTARIATO");
+        titololink.setBounds(45, 50, 600, 40);
+        titololink.setFont(new Font("Verdana", Font.BOLD, 24));
+        JLabel titoloYT=new JLabel("NON GIRARTI DALL'ALTRA PARTE");
+        titoloYT.setBounds(45, 260, 600, 40);
+        titoloYT.setFont(new Font("Verdana", Font.BOLD, 24));
+        easteregg.add(linkYT);easteregg.add(link1);easteregg.add(link2);easteregg.add(titololink);easteregg.add(titoloYT);
         easteregg.setVisible(true);
     }
 
