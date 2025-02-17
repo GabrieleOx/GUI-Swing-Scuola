a70560f69b3fe308e06c8e67396043346a2274af
spoltori <luca.spoltori@gmail.com>
2025-02-17 21:45:12 +0100

    EasterEgg

--- EasterEgg.java
+++ EasterEgg.java
@@ -10,11 +10,11 @@ public class EasterEgg {
         JDialog easteregg = new JDialog((Frame) null, "easteregg", true);
         easteregg.setLayout(null);
         easteregg.setSize(600, 600);
-        easteregg.getContentPane().setBackground(new Color(133, 189, 117));
+        easteregg.getContentPane().setBackground(new Color(120, 66, 100));
 
-        JLabel linkYT = new JLabel("<html><a href=''>Visita il nostro sito</a></html>");
+        JLabel linkYT = new JLabel("<html><a href=''>video di sensibilizzazione</a></html>");
         linkYT.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        linkYT.setBounds(50, 50, 200, 30);
+        linkYT.setBounds(210, 220, 200, 30);
         linkYT.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -25,8 +25,34 @@ public class EasterEgg {
                 }
             }
         });
-
-        easteregg.add(linkYT);
+        JLabel link1 = new JLabel("<html><a href=''>volotariato internazionale</a></html>");
+        link1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        link1.setBounds(210, 150, 200, 30);
+        link1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI("https://www.volunteerworld.com/it"));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        JLabel link2 = new JLabel("<html><a href=''>volotariato per i giovani</a></html>");
+        link2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        link2.setBounds(210, 150, 200, 30);
+        link2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI("https://www.volunteerworld.com/it"));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        
+        easteregg.add(linkYT);easteregg.add(link1);
         easteregg.setVisible(true);
     }
 
@@ -34,3 +60,4 @@ public class EasterEgg {
         SwingUtilities.invokeLater(EasterEgg::showInfo);
     }
 }
+
