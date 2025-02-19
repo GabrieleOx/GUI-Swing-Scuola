783636a0fc5eceed94c9b76a3faa0ca860aef5f1
spoltori <luca.spoltori@gmail.com>
2025-02-19 22:19:30 +0100

    MIGLIORATO UN MINIUMO L'EasterEgg

--- EasterEgg.java
+++ EasterEgg.java
@@ -11,10 +11,11 @@ public class EasterEgg {
         easteregg.setLayout(null);
         easteregg.setSize(600, 600);
         easteregg.getContentPane().setBackground(new Color(133, 189, 117));
-
-        JLabel linkYT = new JLabel("<html><a href=''>video di sensibilizzazione</a></html>");
+        JLabel linkYT = new JLabel("video di sensibilizzazione");
         linkYT.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        linkYT.setBounds(210, 360, 200, 30);
+        linkYT.setBounds(200, 360, 300, 30);
+        linkYT.setFont(new Font("Verdana", Font.BOLD, 15));
+        linkYT.setForeground(Color.WHITE);
         linkYT.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -25,9 +26,11 @@ public class EasterEgg {
                 }
             }
         });
-        JLabel link1 = new JLabel("<html><a href=''>volotariato internazionale</a></html>");
+        JLabel link1 = new JLabel("volotariato internazionale");
         link1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        link1.setBounds(210, 125, 200, 30);
+        link1.setBounds(200, 125, 300, 30);
+        link1.setFont(new Font("Verdana", Font.BOLD, 15));
+        link1.setForeground(Color.WHITE);
         link1.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -38,9 +41,11 @@ public class EasterEgg {
                 }
             }
         });
-        JLabel link2 = new JLabel("<html><a href=''>volotariato per i giovani</a></html>");
+        JLabel link2 = new JLabel("volotariato per i giovani");
         link2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        link2.setBounds(213, 185, 200, 30);
+        link2.setBounds(210, 185, 200, 30);
+        link2.setFont(new Font("Verdana", Font.BOLD, 15));
+        link2.setForeground(Color.WHITE);
         link2.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -55,7 +60,7 @@ public class EasterEgg {
         titololink.setBounds(41, 50, 600, 40);
         titololink.setFont(new Font("Verdana", Font.BOLD, 24));
         JLabel titoloYT=new JLabel("NON GIRARTI DALL'ALTRA PARTE");
-        titoloYT.setBounds(58, 260, 600, 40);
+        titoloYT.setBounds(68, 280, 600, 40);
         titoloYT.setFont(new Font("Verdana", Font.BOLD, 24));
         easteregg.add(linkYT);easteregg.add(link1);easteregg.add(link2);easteregg.add(titololink);easteregg.add(titoloYT);
         easteregg.setVisible(true);
